d1f26d3e8d83b51e60a33771db7014c7cf39c647
alyshae <alison.walas@gmail.com>
2017-07-25 10:44:40 -0700

    added nav bar

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,21 +2,55 @@ console.log("Sanity Check!")
 
 
 $(document).ready(function() {
+
+/********************************
+ *   AJAX REQUESTS to Express   *
+ *******************************/
+
+  //ajax INDEX 'GET' request
   $.ajax({
     method: 'GET',
     url: '/scores',
-    success: handleSuccess,
-    error: handleError
+    success: indexSuccess,
+    error: indexError
   });
 
-  function handleSuccess(jsonData) {
-    jsonData.forEach(function(e) {
-      $('#scores-target').text(`${e.name}`);
+  //ajax NEW 'POST' request
+  $('#newHSForm').on('submit', function(e) {
+    e.preventDefault();
+    $.ajax({
+      method: 'POST',
+      url: '/scores',
+      data: $(this).serialize(),
+      success: newHSSuccess,
+      error: newHSError
     });
+  });
+
+
+
+/*********************************
+ *   SUCCESS & ERROR FUNCTIONS   *
+ ********************************/
+
+  //GET all scores
+  function indexSuccess(jsonData) {
+    el = jsonData[0];
+    $('#scores-target').text(`${el.name}, ${el.highScore}`);
   }
 
-  function handleError(e) {
+  //error with GET all scores
+  function indexError(e) {
     $('#scores-target').text("Failed to load TOP DOGS.")
   }
 
+  //POST new high score
+  function newHSSuccess(jsonData) {
+    console.log('reached new high score success function')
+  }
+
+  //error with POST new high score
+  function newHSError(e) {
+    console.log('error posting new high score: ' + err);
+  }
 }); //end of doc.ready function
