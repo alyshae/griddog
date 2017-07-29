a7044f6c1d9cee792121d555a24ccf8364e1f72a
alyshae <alison.walas@gmail.com>
2017-07-29 10:27:07 -0700

    models/index.js updating mongoose syntax as per server logs.

--- models/index.js
+++ models/index.js
@@ -1,5 +1,9 @@
+//////****     mongoose docs imply use of "var" rather than "let"     ****//////
+
 var mongoose = require("mongoose");
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/griddog");
+var promise = mongoose.connect((process.env.MONGODB_URI || "mongodb://localhost/griddog"), {
+  useMongoClient: true;
+});
 
 //require & export the score model through index.js
 module.exports.Score = require("./score.js");
