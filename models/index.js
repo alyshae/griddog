2ddc0893f31b9a324e8a5d135f5afec8d391e0d5
alyshae <alison.walas@gmail.com>
2017-07-31 14:50:18 -0700

    formatting in models

--- models/index.js
+++ models/index.js
@@ -1,9 +1,9 @@
-//////****     mongoose docs imply use of "var" rather than "let"     ****//////
-
 let mongoose = require("mongoose");
-let promise = mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/griddog", {
-  useMongoClient: true
-});
+let promise = mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/griddog",
+  {
+    useMongoClient: true
+  }
+);
 
 //require & export the score model through index.js
 module.exports.Score = require("./score.js");
